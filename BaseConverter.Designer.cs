bae781a8f124d2fcddc921ae988de4f2d80985b7
Izzat Ariz <izzatariz.2000@gmail.com>
2023-07-04 15:59:31 +0800

    first commit

---
+++ BaseConverter.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace BaseConverter
+{
+    partial class BaseConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblFromBase = new System.Windows.Forms.Label();
+            this.lblToBase = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.cbFromBase = new System.Windows.Forms.ComboBox();
+            this.cbToBase = new System.Windows.Forms.ComboBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNumber.Location = new System.Drawing.Point(35, 45);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(86, 15);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "Enter Number";
+            // 
+            // lblFromBase
+            // 
+            this.lblFromBase.AutoSize = true;
+            this.lblFromBase.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFromBase.Location = new System.Drawing.Point(35, 153);
+            this.lblFromBase.Name = "lblFromBase";
+            this.lblFromBase.Size = new System.Drawing.Size(65, 15);
+            this.lblFromBase.TabIndex = 1;
+            this.lblFromBase.Text = "From Base";
+            // 
+            // lblToBase
+            // 
+            this.lblToBase.AutoSize = true;
+            this.lblToBase.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblToBase.Location = new System.Drawing.Point(35, 258);
+            this.lblToBase.Name = "lblToBase";
+            this.lblToBase.Size = new System.Drawing.Size(49, 15);
+            this.lblToBase.TabIndex = 2;
+            this.lblToBase.Text = "To Base";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResult.Location = new System.Drawing.Point(35, 481);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(42, 15);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumber.Location = new System.Drawing.Point(35, 76);
+            this.txtNumber.Multiline = true;
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(436, 39);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // cbFromBase
+            // 
+            this.cbFromBase.DropDownHeight = 220;
+            this.cbFromBase.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbFromBase.FormattingEnabled = true;
+            this.cbFromBase.IntegralHeight = false;
+            this.cbFromBase.ItemHeight = 30;
+            this.cbFromBase.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36"});
+            this.cbFromBase.Location = new System.Drawing.Point(35, 181);
+            this.cbFromBase.Name = "cbFromBase";
+            this.cbFromBase.Size = new System.Drawing.Size(436, 38);
+            this.cbFromBase.TabIndex = 5;
+            // 
+            // cbToBase
+            // 
+            this.cbToBase.DropDownHeight = 220;
+            this.cbToBase.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbToBase.FormattingEnabled = true;
+            this.cbToBase.IntegralHeight = false;
+            this.cbToBase.ItemHeight = 30;
+            this.cbToBase.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36"});
+            this.cbToBase.Location = new System.Drawing.Point(35, 286);
+            this.cbToBase.Name = "cbToBase";
+            this.cbToBase.Size = new System.Drawing.Size(436, 38);
+            this.cbToBase.TabIndex = 6;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtResult.Location = new System.Drawing.Point(35, 512);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(436, 39);
+            this.txtResult.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(12, 458);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(487, 124);
+            this.panel2.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSwap);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnConvert);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 412);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Base Converter";
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.btnSwap.Location = new System.Drawing.Point(302, 330);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(99, 49);
+            this.btnSwap.TabIndex = 15;
+            this.btnSwap.Text = "Swap";
+            this.btnSwap.UseVisualStyleBackColor = false;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.IndianRed;
+            this.btnReset.Location = new System.Drawing.Point(185, 330);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(99, 49);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnConvert.Location = new System.Drawing.Point(71, 330);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(99, 49);
+            this.btnConvert.TabIndex = 13;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // BaseConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 602);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.cbToBase);
+            this.Controls.Add(this.cbFromBase);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblToBase);
+            this.Controls.Add(this.lblFromBase);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BaseConverter";
+            this.Text = "Base Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblNumber;
+        private Label lblFromBase;
+        private Label lblToBase;
+        private Label lblResult;
+        private TextBox txtNumber;
+        private ComboBox cbFromBase;
+        private ComboBox cbToBase;
+        private TextBox txtResult;
+        private Panel panel2;
+        private GroupBox groupBox1;
+        private Button btnSwap;
+        private Button btnReset;
+        private Button btnConvert;
+    }
+}
---
+++ BaseConverter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BaseConverter
+{
+    public partial class BaseConverter : Form
+    {
+
+        // Variable to hold number, fromBase, toBase value
+        private string input;
+        private int fromBase;
+        private int toBase;
+
+        // Constructor initialise combo box value
+        public BaseConverter()
+        {
+            InitializeComponent();
+            // Set initial combobox selected index
+            cbFromBase.SelectedIndex = 0;
+            cbToBase.SelectedIndex = 0;
+        }
+
+        // Event for convert
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // input, store string from textbox
+            string input = txtNumber.Text;
+
+            // Check if input textbox is empty?
+            if (string.IsNullOrEmpty(input))
+            {
+                // if empty show error
+                MessageBox.Show("Enter Input in the Enter Number Textbox", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // if not empty call convertbase function, and send value from textbox
+                fromBase = int.Parse(cbFromBase.SelectedItem.ToString());
+                toBase = int.Parse(cbToBase.SelectedItem.ToString());
+
+                txtResult.Text = ConvertBase(input, fromBase, toBase);
+            }
+        }
+
+        // Function to convert base
+        public static string ConvertBase(string input, int fromBase, int toBase)
+        {
+
+            // Convert to base 10 first
+            // Variable to hold decimal value after conversion
+            int decimalValue = 0;
+
+            // Iterates through each character in the input string at the index of "i"
+            for (int i = 0; i < input.Length; i++)
+            {
+                // Variable to the numerical value of digit
+                int digitValue;
+                // Checks if the current character is a digit (0-9) using the char.IsDigit() method. 
+                if (char.IsDigit(input[i]))
+                    // To converts the current character to its corresponding integer value
+                    digitValue = input[i] - '0';
+                // If the current character is not a digit, it checks if the character is lowercase using the char.IsLower() method.
+                else if (char.IsLower(input[i]))
+                    // To converts the current character to its corresponding integer value.
+                    digitValue = input[i] - 'a' + 10;
+                else
+                    //If the character is not lowercase, it assumes it is an uppercase letter
+                    // To converts the current character to its corresponding integer value.
+                    digitValue = input[i] - 'A' + 10;
+
+                // Multiplies the current decimal value by the base and adds the digit value to get the new decimal value.
+                decimalValue = decimalValue * fromBase + digitValue;
+            }
+
+            // Converting (base 10) number to a any number of any base in range of (2-36)
+
+            // string output which will be used to store the converted number.
+            string output = "";
+
+            // while loop runs as long as the decimalValue is greater than 0
+            while (decimalValue > 0)
+            {
+                // This part of codes mainly to calculates the remainder of the decimalValue when divided by the target base.
+                // int digitValue will store the remainder 
+                int digitValue = decimalValue % toBase;
+                char digit;
+
+                // if the digit value is less than 10.
+                if (digitValue < 10)
+                    // converts the digit value to its corresponding character representation by adding the ASCII value of the character '0' to the digit value.
+                    digit = (char)('0' + digitValue);
+                else
+                    // Digit value is not less than 10, it assumes it is a letter, between 10 and 15 (a-f),
+                    // Converts the digit value to its corresponding character representation
+                    digit = (char)('A' + digitValue - 10);
+
+                // adds the digit to the output string
+                output = digit + output;
+                // divides the decimal value by the target base using the /= operator, updating the value of decimalValue for the next iteration.
+                decimalValue /= toBase;
+            }
+            // The output string is returned.
+            return output;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtNumber.Clear();
+            txtResult.Clear();
+            cbFromBase.SelectedIndex = 0;
+            cbToBase.SelectedIndex = 0;
+        }
+
+        private void btnSwap_Click(object sender, EventArgs e)
+        {
+            string temp;
+            string fromBase, toBase;
+
+            fromBase = cbFromBase.Text;
+            toBase = cbToBase.Text;
+
+            temp = cbFromBase.Text;
+            fromBase = cbToBase.Text;
+            toBase = temp;
+
+            cbFromBase.Text = fromBase;
+            cbToBase.Text = toBase;
+        }
+    }
+}
